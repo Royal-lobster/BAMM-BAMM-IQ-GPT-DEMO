f148cf40c5ffdbc7cb2585de8d4833058d28ed59
Srujan Gurram <srujangs8@gmail.com>
2024-01-29 20:06:32 +0530

    Update Hero component to adjust the font size of the heading and add responsive flex classes to the container and button elements

--- app/(home)/Hero.tsx
+++ app/(home)/Hero.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export function Hero() {
 	return (
 		<section className="bg-gradient-to-b py-10 from-gray-950 to-gray-900">
-			<div className="flex items-center container mx-auto">
+			<div className="flex items-center container mx-auto flex-wrap">
 				<div className="flex flex-wrap flex-col w-full md:w-1/2 p-4 text-center md:text-left">
-					<h1 className="text-5xl font-bold mb-4">
+					<h1 className="text-4xl md:text-5xl font-bold mb-4">
 						Blast Off to Meme Stardom with BAMM-BAMM!
 					</h1>
 					<p className="mb-6">
@@ -14,7 +14,7 @@ export function Hero() {
 						token that's all about the LOLs and the gains. Join us, and let's
 						rocket to the moon – meme style!
 					</p>
-					<div className="flex gap-2">
+					<div className="flex justify-center md:justify-start flex-wrap gap-2">
 						<Button>Get Your Meme On!</Button>
 						<Button variant="secondary">The BAMM-BAMM Lowdown</Button>
 					</div>
